0b63e245983622c6a2a8e8f061bfd52b2506bd0a
Roopa Shivappa <roopa.shivappa@autodesk.com>
2017-02-24 23:34:55 +0800

    Add logging functionality

--- src/test/java/com/leantrips/tests/LoginTest.java
+++ src/test/java/com/leantrips/tests/LoginTest.java
@@ -4,6 +4,8 @@ import static org.testng.Assert.fail;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
@@ -15,74 +17,80 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://www.leantrips.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	private static final Logger logger = LogManager.getLogger(LoginTest.class);
 
-  @Test
-  public void testLogin() throws Exception {
-    driver.get(baseUrl + "/");
-    driver.findElement(By.linkText("Log In")).click();
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.id("inputEmail")).clear();
-    driver.findElement(By.id("inputEmail")).sendKeys("test1@test.com");
-    driver.findElement(By.id("inputPassword")).clear();
-    driver.findElement(By.id("inputPassword")).sendKeys("Password123");
-    driver.findElement(By.linkText("Log Out")).click();
-    driver.findElement(By.id("inputEmail")).clear();
-    driver.findElement(By.id("inputEmail")).sendKeys("test1@test.com");
-    driver.findElement(By.id("inputPassword")).clear();
-    driver.findElement(By.id("inputPassword")).sendKeys("Password123");
-  }
+	@BeforeClass(alwaysRun = true)
+	public void setUp() throws Exception {
+		logger.debug("Start setup");
+		driver = new FirefoxDriver();
+		baseUrl = "http://www.leantrips.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@Test
+	public void testLogin() throws Exception {
+		logger.debug("Start testLogin");
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+		driver.get(baseUrl + "/");
+		driver.findElement(By.linkText("Log In")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.id("inputEmail")).clear();
+		driver.findElement(By.id("inputEmail")).sendKeys("test1@test.com");
+		driver.findElement(By.id("inputPassword")).clear();
+		driver.findElement(By.id("inputPassword")).sendKeys("Password123");
+		driver.findElement(By.linkText("Log Out")).click();
+		driver.findElement(By.id("inputEmail")).clear();
+		driver.findElement(By.id("inputEmail")).sendKeys("test1@test.com");
+		driver.findElement(By.id("inputPassword")).clear();
+		driver.findElement(By.id("inputPassword")).sendKeys("Password123");
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	@AfterClass(alwaysRun = true)
+	public void tearDown() throws Exception {
+		logger.debug("Teardown");
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
